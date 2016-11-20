c45f92e53468616ce1fa693d4c804a7e40ba7051
Manpreet_Singh <manpreet01@outlook.com>
2016-11-19 23:40:53 -0800

    passing multiple xml values

--- nvd/scripts/nvd_extract.pl
+++ nvd/scripts/nvd_extract.pl
@@ -12,17 +12,19 @@ use lib qq($FindBin::Bin/../lib);
 use NVD;
 use Dumpvalue;
 
-if (@ARGV != 1) {
+if (@ARGV == 0) {
     die "Extract NVD vulnerability records into a SQLite database file\n",
         "Usage:\n    $0 <NVD_XML_files>\n\n";
 }
-
+#my $size = @ARGV;
+#print "$size\n"
 foreach my $file  (@ARGV){
- #print "$a"
- die "No file $file " if not -e $file;
- my %vuln = NVD::extract($file);
- Dumpvalue->new->dumpValue(\%vuln);
+ print "$file\n"
+ #die "No file $file " if not -e $file;
+ #my %vuln = NVD::extract($file);
+ #Dumpvalue->new->dumpValue(\%vuln);
 }
+
 
 
 #my %vuln = NVD::extract(@ARGV);
